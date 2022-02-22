a90376eafc6249d8c239b144f8d3dfb69aaffadc
VivianeAraujo <vyviaraujo@hotmail.com.br>
2022-02-21 22:51:57 -0300

    Update introducao_sintaxe.py

--- introducao_sintaxe.py
+++ introducao_sintaxe.py
@@ -11,18 +11,7 @@ Enunciado: Introdução a sintaxe e tipos de dados"""
 
 
 
-Aluno = "Viviane Araujo"
+nome = "viviane Araujo Vieira Costa"
 
-
-nota1 = float(input("Digite a primeira nota: "))
-
-nota2 = float(input("Digite a segunda nota: "))
-
-nota3 = float(input ("Digite a terceira nota: "))
-
-media = (nota1 + nota2 + nota3) / 3
-
-media_arred = round(media)
-
-print(f"\no aluno {Aluno} ficou com a média {media_arred}")
+print(f"meu nome é {nome.count()}")
 
