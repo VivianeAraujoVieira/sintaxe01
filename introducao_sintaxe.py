98c4193e93816d91cc428ba066620c6e6fc5191a
VivianeAraujo <vyviaraujo@hotmail.com.br>
2022-02-21 23:35:34 -0300

    introdução sintaxe
    correção de dados apagados

--- introducao_sintaxe.py
+++ introducao_sintaxe.py
@@ -1,22 +1,20 @@
-##############################################################################
-# Parte do livro Introdução à Programação com Python
-# Autor: Nilo Ney Coutinho Menezes
-# Editora Novatec (c) 2010-2020
-# Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
-# Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
-# Terceira Edição - Janeiro/2019 - ISBN 978-85-7522-718-3
-#
-# Site: https://python.nilo.pro.br/
-#
-# Arquivo: exercicios3\capitulo 02\exercicio-02-01.py
-##############################################################################
+"""
+Como Bootcamps - Stone - /código[s]
+Data: 17/02/2022
+Autor: Viviane Araujo Vieira
+Enunciado: Introdução aos tipos de dados (string)
+"""
 
-# Para executar o cálculo e visualizar a resposta,
-# copie e cole as linhas abaixo para a janela do interpretador,
-# uma de cada vez.
-# As respostas do exercício são as linhas abaixo:
-10 + 20 * 30
-4 ** 2 / 30
-(9 ** 4 + 2) * 6 - 1
+aluna = "Viviane Araujo Vieira da costa"
 
+nota1 = float(input("Digite a primeira nota: "))
 
+nota2 = float(input("Digite a segunda nota: "))
+
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+media_arred = round(media, 2)
+
+print(f"\nA aluna {aluna} ficou com a média {media_arred}")
